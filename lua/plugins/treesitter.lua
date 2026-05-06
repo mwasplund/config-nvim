74ff212f754d6dabcfb3384fd6a07f8b61864094
masplund <masplund@microsoft.com>
2026-05-05 17:44:53 -0700

    Fix highlight and install:

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,35 @@
 return {
   'nvim-treesitter/nvim-treesitter',
-  lazy = false,
-  build = ':TSUpdate'
+  build = ':TSUpdate',
+  config = function()
+    local treesitter = require("nvim-treesitter")
+    treesitter.setup()
+    treesitter.install({
+      "lua",
+      "javascript",
+      "typescript",
+      "html",
+      "css",
+      "python",
+      "c",
+      "cpp",
+      "json",
+      "bash",
+      "markdown",
+      "markdown_inline",
+    })
+
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = { 'java', 'c', 'lua', 'vim', 'vimdoc', 'query', 'elixir', 'heex', 'javascript', 'typescript', 'html', 'yaml' },
+      callback = function()
+        -- syntax highlighting, provided by Neovim
+        vim.treesitter.start()
+        -- folds, provided by Neovim (I don't like folds)
+        -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+        -- vim.wo.foldmethod = 'expr'
+        -- indentation, provided by nvim-treesitter
+        -- TODO: vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+  end,
 }
