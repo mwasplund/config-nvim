049aa761f3ea0c4931cabca03c84990f92ab7648
masplund <masplund@microsoft.com>
2026-05-05 19:39:37 -0700

    Add telescope

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc="Open parent directory in Oil" })
 
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
 vim.keymap.set("n", "<leader>cX", ":%s/\\r//g<CR>", { noremap = true, desc = "Remove ^M char" })
