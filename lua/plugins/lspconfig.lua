2463f96007ada369e966cce29eed0dab9bb7fea9
masplund <masplund@microsoft.com>
2026-05-06 21:13:41 -0700

    fix yaml

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -80,7 +80,7 @@ local servers = {
    --    https://github.com/pmizio/typescript-tools.nvim
    --
    -- But for many setups, the LSP (`ts_ls`) will work just fine
-   -- ts_ls = {},
+   ts_ls = {},
 
    -- stylua = {}, -- Used to format Lua code
 
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,70 +4,71 @@ vim.pack.add({
   -- Incremental selection
   gh 'shushtain/incselect.nvim',
 })
-      local treesitter = require("nvim-treesitter")
-      treesitter.install({
-        "lua",
-        "javascript",
-        "typescript",
-        "html",
-        "css",
-        "python",
-        "c",
-        "cpp",
-        "json",
-        "bash",
-        "markdown",
-        "markdown_inline",
-      })
+local treesitter = require("nvim-treesitter")
+treesitter.install({
+  "lua",
+  "javascript",
+  "typescript",
+  "html",
+  "css",
+  "python",
+  "c",
+  "cpp",
+  "json",
+  "bash",
+  "markdown",
+  "markdown_inline",
+  "yaml",
+})
 
-      vim.api.nvim_create_autocmd('FileType', {
-        pattern = { 'java', 'c', 'cpp', 'lua', 'vim', 'vimdoc', 'query', 'elixir', 'heex', 'javascript', 'typescript', 'html', 'yaml' },
-        callback = function()
-          -- syntax highlighting, provided by Neovim
-          vim.treesitter.start()
-          -- folds, provided by Neovim (I don't like folds)
-          -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-          -- vim.wo.foldmethod = 'expr'
-          -- indentation, provided by nvim-treesitter
-          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-        end,
-      })
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'java', 'c', 'cpp', 'lua', 'vim', 'vimdoc', 'query', 'elixir', 'heex', 'javascript', 'typescript', 'html', 'yaml' },
+  callback = function()
+    -- syntax highlighting, provided by Neovim
+    vim.treesitter.start()
+    -- folds, provided by Neovim (I don't like folds)
+    -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+    -- vim.wo.foldmethod = 'expr'
+    -- indentation, provided by nvim-treesitter
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+  end,
+})
 
-      -- Disable entire built-in ftplugin mappings to avoid conflicts.
-      -- See https://github.com/neovim/neovim/tree/master/runtime/ftplugin for built-in ftplugins.
-      vim.g.no_plugin_maps = true
+-- Disable entire built-in ftplugin mappings to avoid conflicts.
+-- See https://github.com/neovim/neovim/tree/master/runtime/ftplugin for built-in ftplugins.
+vim.g.no_plugin_maps = true
 
-      -- Or, disable per filetype (add as you like)
-      -- vim.g.no_python_maps = true
-      -- vim.g.no_ruby_maps = true
-      -- vim.g.no_rust_maps = true
-      -- vim.g.no_go_maps = true
-     
+-- Or, disable per filetype (add as you like)
+-- vim.g.no_python_maps = true
+-- vim.g.no_ruby_maps = true
+-- vim.g.no_rust_maps = true
+-- vim.g.no_go_maps = true
+
 require("nvim-treesitter-textobjects").setup {
-        select = {
-          -- Automatically jump forward to textobj, similar to targets.vim
-          lookahead = true,
-          -- You can choose the select mode (default is charwise 'v')
-          --
-          -- Can also be a function which gets passed a table with the keys
-          -- * query_string: eg '@function.inner'
-          -- * method: eg 'v' or 'o'
-          -- and should return the mode ('v', 'V', or '<c-v>') or a table
-          -- mapping query_strings to modes.
-          selection_modes = {
-            ['@parameter.outer'] = 'v', -- charwise
-            ['@function.outer'] = 'V', -- linewise
-            -- ['@class.outer'] = '<c-v>', -- blockwise
-          },
-          -- If you set this to `true` (default is `false`) then any textobject is
-          -- extended to include preceding or succeeding whitespace. Succeeding
-          -- whitespace has priority in order to act similarly to eg the built-in
-          -- `ap`.
-          --
-          -- Can also be a function which gets passed a table with the keys
-          -- * query_string: eg '@function.inner'
-          -- * selection_mode: eg 'v'
-          -- and should return true of false
-          include_surrounding_whitespace = false,
-        },
-      }
+  select = {
+    -- Automatically jump forward to textobj, similar to targets.vim
+    lookahead = true,
+    -- You can choose the select mode (default is charwise 'v')
+    --
+    -- Can also be a function which gets passed a table with the keys
+    -- * query_string: eg '@function.inner'
+    -- * method: eg 'v' or 'o'
+    -- and should return the mode ('v', 'V', or '<c-v>') or a table
+    -- mapping query_strings to modes.
+    selection_modes = {
+      ['@parameter.outer'] = 'v', -- charwise
+      ['@function.outer'] = 'V', -- linewise
+      -- ['@class.outer'] = '<c-v>', -- blockwise
+    },
+    -- If you set this to `true` (default is `false`) then any textobject is
+    -- extended to include preceding or succeeding whitespace. Succeeding
+    -- whitespace has priority in order to act similarly to eg the built-in
+    -- `ap`.
+    --
+    -- Can also be a function which gets passed a table with the keys
+    -- * query_string: eg '@function.inner'
+    -- * selection_mode: eg 'v'
+    -- and should return true of false
+    include_surrounding_whitespace = false,
+  },
+}
